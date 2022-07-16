d4eadf19a5dcad64f040983ed9eecd61a9a335b2
AL Reasat Rafio <rafioar96@gmail.com>
2022-07-16 20:09:00 +0600

    :hammer: first_cpp node added

--- build/my_py_pkg/build/lib/my_py_pkg/my_first_node.py
+++ build/my_py_pkg/build/lib/my_py_pkg/my_first_node.py
@@ -2,10 +2,18 @@ import rclpy
 from rclpy.node import Node
 
 
+class MyNode(Node):
+    def __init__(self):
+        super().__init__("py_test")
+        self.create_timer(0.5, self.timer_callback)
+
+    def timer_callback(self):
+        self.get_logger().info("Helloo")
+
+
 def main(args=None):
     rclpy.init(args=args)
-    node = Node("py_test")
-    node.get_logger().info("hello ros2")
+    node = MyNode()
     rclpy.spin(node)
     rclpy.shutdown()
 
