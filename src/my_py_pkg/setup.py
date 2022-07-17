b37b69d426125e53393c9da115b864d10d18075f
AL Reasat Rafio <rafioar96@gmail.com>
2022-07-17 19:10:37 +0600

    :hammer: python client oop version added

--- src/my_py_pkg/setup.py
+++ src/my_py_pkg/setup.py
@@ -25,6 +25,7 @@ setup(
             "smartphone = my_py_pkg.smartphone:main",
             "add_two_ints_server = my_py_pkg.add_two_ints_server:main",
             "add_two_ints_client_no_oop = my_py_pkg.add_two_ints_client_no_oop:main",
+            "add_two_ints_client = my_py_pkg.add_two_ints_client:main",
         ],
     },
 )
